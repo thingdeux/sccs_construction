29341b5b513dacfe00674e6f65abc23891768a66
Joshua Johnson <me@josh.land>
2014-07-07 01:03:25 -0700

    Initial Commit

---
+++ Construction/settings.py
@@ -0,0 +1,89 @@
+"""
+Django settings for Construction project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0x01g*78ynk9%ebmxe5-2jcfxp7y5xnx&o@00ebx0)rc=a+z@l'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'bootstrap_admin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',    
+    'quote',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'Construction.urls'
+
+WSGI_APPLICATION = 'Construction.wsgi.application'
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+#Requires
+#django-xadmin
---
+++ quote/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from quote.models import Quote
+
+# Register your models here.
+class QuoteAdmin(admin.ModelAdmin):
+    fields = ("first_name", "last_name", ("email", "phone"),"date_requested", "requiresResponse", "comments")
+    #readonly_fields = ('email','phone')
+    #readonly_fields = ('date_requested')
+
+    list_display = ('email', 'first_name', 'last_name', 
+                    'date_requested', 'requiresResponse')
+    search_fields = ('last_name',)    
+
+admin.site.register(Quote, QuoteAdmin)
