f6e321f60db9e164a2dc6c499dd1a645358c05ee
Joshua Johnson <me@josh.land>
2014-07-08 17:14:54 -0700

    Design #1 complete.

---
+++ quotes/static/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){           
+    $('.testimonials').slick({
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 2500,
+        dots: false,
+        pauseOnHover: true,
+        useCSS: true,
+    });
+});
