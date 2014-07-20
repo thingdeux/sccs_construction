4ce403cb0131dd6e179a03ffb20d6b4e114f6441
Joshua Johnson <me@josh.land>
2014-07-19 23:20:50 -0700

    Fixed thanks template - updated e-mail text with dev url.

--- Construction/wsgi_prod.py
+++ Construction/wsgi_prod.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'construction.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Construction.settings'
 
 from django.core.handlers.wsgi import WSGIHandler
 
